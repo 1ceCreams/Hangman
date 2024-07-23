07fa512bcc05ab39633fa3a086f99c494a4d4628
Aleksey <black-colorss@mail.ru>
2024-07-24 01:11:33 +0300

    Рабочая игра

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -5,44 +5,63 @@ import java.util.Scanner;
 
 public class Game {
     private final GameStates states;
+    private final short MAX_ERRORS = 6;
     private String word;
     private boolean gameover = false;
+    StringBuilder visibleWord;
 
 
     public Game(){
         states = new GameStates();
-        try {
-            word = new WordSelector().getWord("src/main/resources/russian_nouns.txt");
-        }catch (IOException e){
-            System.out.println("Файл не найден");
-        }
     }
 
-    public void run(){
+    public void run() throws IOException {
+        word = new WordSelector().getWord("src/main/resources/russian_nouns.txt");
         Scanner in = new Scanner(System.in);
-        StringBuilder visibleWord = new StringBuilder();
+        gameover = false;
+        visibleWord = new StringBuilder();
         char userLatter;
         char[] chars = word.toCharArray();
+        boolean letterFound;
+        boolean win = false;
+        short errorCounter = 0;
 
         for (int i = 0; i < word.length(); i++){
             visibleWord.append("_");
         }
-        System.out.println(states.getStates().get(0));
-        System.out.println(visibleWord.toString());
         while(!gameover){
+            System.out.println(states.getStates().get(errorCounter));
+            System.out.println(visibleWord.toString());
+            if(errorCounter == MAX_ERRORS){
+                System.out.println("Вы проиграли\n загаданное слово:"+word);
+                gameover = true;
+                continue;
+            }
             System.out.println("Введите букву");
             int letterIndex = 0;
             userLatter = in.next().charAt(0);
             if(userLatter == ' '){
                 continue;
             }
+            letterFound = false;
             for (char letter:chars){
                 if(letter == userLatter){
                     visibleWord.setCharAt(letterIndex,userLatter);
+                    letterFound = true;
+                }
+                if(visibleWord.indexOf(String.valueOf('_')) == -1){
+                    win = true;
                 }
                 letterIndex++;
             }
-            System.out.println(visibleWord);
+            if(!letterFound){
+                errorCounter++;
+            }
+            if(win){
+                System.out.println("Вы победили");
+                break;
+            }
+
         }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,44 @@
 package org.example;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Game game = new Game();
-        game.run();
+        boolean exit = false;
+        Scanner scanner;
+
+        while(!exit){
+            int choice;
+            scanner = new Scanner(System.in);
+            System.out.println("Выберите пункт меню");
+            System.out.println("1. Начать игру");
+            System.out.println("2. Выйти");
+            try {
+                choice = scanner.nextInt();
+            }
+            catch (InputMismatchException e ){
+                System.out.println("Такого варианта нет");
+                continue;
+            }
+            switch (choice){
+                case 1:{
+                    game.run();
+                    break;
+                }
+                case 2:{
+                    exit = true;
+                    break;
+                }
+                default:{
+                    System.out.println("Такого варианта нет");
+                    break;
+                }
+            }
+        }
 
     }
 }
