ce918f8c78dc0389ad862c273fe03fb01fb2b607
Aleksey <black-colorss@mail.ru>
2024-07-23 22:26:38 +0300

    Начало разработки

---
+++ src/main/java/org/example/GameStates.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameStates {
+    List<String> states;
+
+    public GameStates(String[] states){
+        this.states = new ArrayList<String>();
+        for (String state : states){
+            states.
+        }
+    }
+}
